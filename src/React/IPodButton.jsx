b793f0fa8872b08da728364282bc611d64c187bb
Kapish Verma <122527683+kapishverma@users.noreply.github.com>
2023-06-28 16:10:08 +0530

    Update IPodButton.jsx

--- src/React/IPodButton.jsx
+++ src/React/IPodButton.jsx
@@ -8,14 +8,14 @@ export default function IPodButton(props) {
     return (<>
         <div className={CSS.buttonSide}>
             <div className={CSS.buttonOuter} ref={wheelRef}>
-                <button className={CSS.button + ' ' + CSS.menuButton} onClick={() => btnFunction.menuOrBack()}>MENU</button>
-                <button className={CSS.button + ' ' + CSS.back} onClick={() => btnFunction.backwardBtn()}><i className="bi bi-skip-backward-fill"></i></button>
-                <button className={CSS.button + ' ' + CSS.forward} onClick={() => btnFunction.forwardBtn()}><i className="bi bi-skip-forward-fill"></i></button>
-                <button className={CSS.button + ' ' + CSS.playPause} onClick={() => btnFunction.playBtn()}><i className="bi bi-play-fill"></i><i className="bi bi-pause-fill"></i></button>
+                <button className={CSS.button + ' ' + CSS.menuButton} onTouchEnd={() => btnFunction.menuOrBack()} onClick={() => btnFunction.menuOrBack()}>MENU</button>
+                <button className={CSS.button + ' ' + CSS.back} onTouchEnd={() => btnFunction.backwardBtn()} onClick={() => btnFunction.backwardBtn()}><i className="bi bi-skip-backward-fill"></i></button>
+                <button className={CSS.button + ' ' + CSS.forward} onTouchEnd={() => btnFunction.forwardBtn()} onClick={() => btnFunction.forwardBtn()}><i className="bi bi-skip-forward-fill"></i></button>
+                <button className={CSS.button + ' ' + CSS.playPause} onTouchEnd={() => btnFunction.playBtn()} onClick={() => btnFunction.playBtn()}><i className="bi bi-play-fill"></i><i className="bi bi-pause-fill"></i></button>
             </div>
-            <div className={CSS.buttonInner} onClick={() => btnFunction.okButtonHandle(selectedOption)}>
+            <div className={CSS.buttonInner} onTouchEnd={() => btnFunction.okButtonHandle(selectedOption)} onClick={() => btnFunction.okButtonHandle(selectedOption)}>
                 <h1>OK</h1>
-            </div> 
+            </div>
         </div>
-    </>) 
+    </>)
 }
